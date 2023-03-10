33c76c47c24183f60e10bb3984561168872a7aff
Julian Mayes <juliantmayes@gmail.com>
2023-03-10 10:18:02 -0800

    clients copy

--- components/Plans/data.js
+++ components/Plans/data.js
@@ -13,7 +13,7 @@ import {
 
 export const links = [
   {
-    head: "Basic",
+    head: "Standard",
     price: "$499",
     text: "",
     url: "/",
@@ -39,11 +39,15 @@ export const links = [
     url: "/",
     list: [
       {
-        head: "Everything in Basic Package",
+        head: "Standard Package",
         icon: <CheckCircle alt="icon" />,
       },
       {
-        head: "Content Creation (Photography and videography. 4 post/wk)",
+        head: "Content Creation (Photography and videography. 3-4 post/wk)",
+        icon: <CheckCircle alt="icon" />,
+      },
+      {
+        head: "Multiple social media platforms (up to 3)",
         icon: <CheckCircle alt="icon" />,
       },
       {
@@ -55,40 +59,44 @@ export const links = [
         icon: <CheckCircle alt="icon" />,
       },
       {
-        head: "Monthly Analytics (social media, website) ",
+        head: "Monthly Analytics (social media, website)",
+        icon: <CheckCircle alt="icon" />,
+      },
+      {
+        head: "Influencer marketing/proper outreach",
         icon: <CheckCircle alt="icon" />,
       },
     ],
   },
-  {
-    head: "Pro+",
-    price: "Inquire",
-    text: "",
-    style: "fill",
-    url: "/",
-    list: [
-      {
-        head: "Everything in Pro Package",
-        icon: <CheckCircle alt="icon" />,
-      },
-      {
-        head: "Content Creation (Photography and videography. 4 post/wk)",
-        icon: <CheckCircle alt="icon" />,
-      },
-      {
-        head: "Multiple social media platforms",
-        icon: <CheckCircle alt="icon" />,
-      },
-      {
-        head: "Events",
-        icon: <CheckCircle alt="icon" />,
-      },
-      {
-        head: "Influencer marketing/ proper outreach",
-        icon: <CheckCircle alt="icon" />,
-      },
-    ],
-  },
+  // {
+  //   head: "Pro+",
+  //   price: "Inquire",
+  //   text: "",
+  //   style: "fill",
+  //   url: "/",
+  //   list: [
+  //     {
+  //       head: "Everything in Pro Package",
+  //       icon: <CheckCircle alt="icon" />,
+  //     },
+  //     {
+  //       head: "Content Creation (Photography and videography. 4 post/wk)",
+  //       icon: <CheckCircle alt="icon" />,
+  //     },
+  //     {
+  //       head: "Multiple social media platforms",
+  //       icon: <CheckCircle alt="icon" />,
+  //     },
+  //     {
+  //       head: "Events",
+  //       icon: <CheckCircle alt="icon" />,
+  //     },
+  //     {
+  //       head: "Influencer marketing/ proper outreach",
+  //       icon: <CheckCircle alt="icon" />,
+  //     },
+  //   ],
+  // },
   {
     head: "Custom",
     price: "Inquire",
@@ -97,6 +105,18 @@ export const links = [
     list: [
       {
         head: "A personalized package built specifically for you and your business",
+        icon: <CheckCircle alt="icon" />,
+      },
+      {
+        head: "Events planning",
+        icon: <CheckCircle alt="icon" />,
+      },
+      {
+        head: "Logo design",
+        icon: <CheckCircle alt="icon" />,
+      },
+      {
+        head: "Menu/Poster Design",
         icon: <CheckCircle alt="icon" />,
       },
     ],
